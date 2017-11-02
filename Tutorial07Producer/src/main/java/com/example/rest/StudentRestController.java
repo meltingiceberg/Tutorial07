8520f270f4c750d95f102faff0b09016f88cdcf7
Sukrian Syahnel Putra <rian.syahnel@gmail.com>
2017-11-02 09:27:27 +0700

    implement viewall for rest

--- Tutorial07Producer/src/main/java/com/example/rest/StudentRestController.java
+++ Tutorial07Producer/src/main/java/com/example/rest/StudentRestController.java
@@ -1,5 +1,7 @@
 package com.example.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +20,11 @@ public class StudentRestController {
 	public StudentModel view (@PathVariable(value = "npm") String npm) {
 		StudentModel student = studentService.selectStudent(npm);
 		return student;
+	}
+	
+	@RequestMapping("/student/viewall")
+	public List<StudentModel> viewAll() {
+		List<StudentModel> students = studentService.selectAllStudents();
+		return students;
 	}
 }
